82b053a909ade11c51e29e669e41ba18c1d1f861
Douglas Heatherly <d.heatherly@verizon.net>
2014-07-10 17:01:12 -0500

    removed the message line from script for testing

--- cachematrix.R
+++ cachematrix.R
@@ -34,7 +34,6 @@ cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
     if(!is.null(inv)) {
-        message("getting cached data")
         return(inv)
     }
     data <- x$get()
